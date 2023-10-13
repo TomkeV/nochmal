f4913bb01df3351a09c779bf2ea30fb97063ee08
TomkeV <lotvel03@gmail.com>
2023-10-13 09:10:18 +0200

    erster Commit (Versuch)

---
+++ src/main/scala/de/htwg/se/nochmal/Nochmal.scala
@@ -0,0 +1,27 @@
+
+//Zeilen: lines / Spalten: columns / Spielfeld: pitch
+@main def nochmal: Unit =
+  println("Willkommen bei Nochmal!")
+  println(pitch())
+
+val eol = sys.props("line.separator")
+//val bezeichnung = "  A   B   C   D   E   F" + eol
+
+def title(cellWidth:Int = 3, colNum:Int = 7) = 
+  (" " + (" " * ((cellWidth-1)/2)) + "A" + " ") * colNum + eol
+
+def columns(cellWidth:Int = 3, colNum:Int = 7) = 
+  ("+" + "-" * cellWidth) * colNum + "+" + eol
+
+def lines(cellWidth:Int = 3, colNum:Int = 7) = 
+  ("|" + " " * cellWidth) * colNum + "|" + eol
+
+def pointsFirst(cellWidth:Int = 3, colNum:Int = 7) =
+  (" " + (" " * ((cellWidth-1)/2)) + "5" + " ") * colNum + eol
+
+def pointsNext(cellWidth:Int = 3, colNum:Int = 7) =
+  (" " + (" " * ((cellWidth-1)/2)) + "3" + " ") * colNum + eol
+
+def pitch(cellWidth:Int = 3, colNum:Int = 7, lineNum:Int = 4) = 
+  title(cellWidth, colNum) + (columns(cellWidth, colNum) + lines(cellWidth, colNum)) * lineNum + 
+  columns(cellWidth, colNum) + pointsFirst(cellWidth, colNum) + pointsNext(cellWidth, colNum)
