68bbe6163ce303f3b516b828203186a0e69e747e
TomkeV <lotvel03@gmail.com>
2023-11-03 06:28:25 +0100

    added test for class

--- src/test/scala/de/htwg/se/nochmal/Bastelkiste/SpielfeldSpec.scala
+++ src/test/scala/de/htwg/se/nochmal/Bastelkiste/SpielfeldSpec.scala
@@ -11,33 +11,29 @@ val eol = sys.props("line.separator")
 class SpielfeldSpec extends AnyWordSpec {
   "Spielfeld" should {
     val testfeld = meinFeld()
-    "have a bar as String of Form '+---+---+---+---+---+---+---+' " in {
+    "have a bar as String of Form '+---+---+---+---+---+---+---+' when its created without Parameters" in {
       testfeld.columns() should be("+---+---+---+---+---+---+---+" + eol)
     }
-
-    "have cells as String of form '|   |   |   |   |   |   |   |' " in {
+    "have cells as String of form '|   |   |   |   |   |   |   |' when its created without Parameters" in {
       testfeld.lines() should be("|   |   |   |   |   |   |   |" + eol)
     }
     
-/*     "have a scalable bar" in {
-      columns(1, 1) should be("+-+" + eol)
-      columns(1, 2) should be("+-+-+" + eol)
-      columns(2, 1) should be("+--+" + eol)
+    val mini_testfeld = meinFeld(1, 1, 1)
+    "have a bar in String of Form '+-+' when its created with one column and minimal boxsize" in {
+      mini_testfeld.columns() should be("+-+" + eol)
     }
-    "have scalable cells" in {
-      lines(1, 1) should be("| |" + eol)
-      lines(1, 2) should be("| | |" + eol)
-      lines(2, 1) should be("|  |" + eol)
+    "have a cell in String of Form '| |' when its created with one line and minimal boxsize" in {
+      mini_testfeld.lines() should be("| |" + eol)
     }
-
     "have a pitch in the form " + 
       " A " +
       "+-+" +
       "| |" + 
       "+-+" + 
       " 5 " +
-      " 3" in {
-        pitch(1, 1, 1) should be(" A " + eol + "+-+" + eol + "| |" + eol + "+-+" + eol + " 5 " + eol + " 3 " + eol)
-      } */
+      " 3"  +
+      "when its created in minimum size "in {
+      mini_testfeld.pitchToString() should be(" A " + eol + "+-+" + eol + "| |" + eol + "+-+" + eol + " 5 " + eol + " 3 " + eol)
+    }
   }
 }
