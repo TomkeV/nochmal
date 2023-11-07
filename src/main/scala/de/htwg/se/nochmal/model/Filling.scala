7b8bb9363e4b674613967ef949dcc5b8ccd4eabf
TomkeV <lotvel03@gmail.com>
2023-11-07 08:37:58 +0100

    put the enum into an own class

---
+++ src/main/scala/de/htwg/se/nochmal/model/Filling.scala
@@ -0,0 +1,8 @@
+package de.htwg.se.nochmal
+package model
+
+// --- Enum Filling ---
+enum Filling(s:String):
+  override def toString(): String = s
+  case filled extends Filling("X")
+  case empty extends Filling("_")
