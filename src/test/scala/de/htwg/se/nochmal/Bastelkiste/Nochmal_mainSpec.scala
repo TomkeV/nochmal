b5af3ba8fc5ab5d7ee984e922e4e4699849d2228
TomkeV <lotvel03@gmail.com>
2023-11-07 08:43:52 +0100

    added test for main but does not work

---
+++ src/test/scala/de/htwg/se/nochmal/Bastelkiste/Nochmal_mainSpec.scala
@@ -0,0 +1,15 @@
+/* package de.htwg.se.nochmal
+package Bastelkiste
+
+import org.scalatest.wordspec.AnyWordSpec
+import org.scalatest.matchers.should.Matchers._
+
+class Nochmal_mainSpec extends AnyWordSpec {
+    "Nochmal_main" should {
+        "create a new pitch using start_game" in {
+            start_game() should be(pitch()) // hängt sich auf wg Nutzereingabe
+        }
+    }
+}
+
+ */
