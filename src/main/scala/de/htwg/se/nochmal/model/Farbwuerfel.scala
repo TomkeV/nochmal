55f5121beb5a8454f0b953bf9fb5a7b11bcd3b36
TomkeV <lotvel03@gmail.com>
2023-11-10 10:31:45 +0100

    added dice-classes

---
+++ src/main/scala/de/htwg/se/nochmal/model/Farbwuerfel.scala
@@ -0,0 +1,28 @@
+package de.htwg.se.nochmal
+package model
+
+case class colors_dice(anzahl:Int = 3):
+  val wuerfelzahl = anzahl
+  
+  def roll_dice(): String =
+    if (anzahl < 1) {
+      return "Du brauchst mindestens einen Wuerfel!"
+    } else {
+      val randomizer = new scala.util.Random
+      val n = (1 to anzahl).toList
+      val diced_colors = n.map(i => randomizer.nextInt(6))
+      
+      val eol = sys.props("line.separator")
+      var wuerfelergebnis = ""
+      for (y <- diced_colors) {
+        y match {
+        case 0 => wuerfelergebnis += "rot" + eol
+        case 1 => wuerfelergebnis += "orange" + eol
+        case 2 => wuerfelergebnis += "gelb" + eol
+        case 3 => wuerfelergebnis += "grün" + eol
+        case 4 => wuerfelergebnis += "blau" + eol
+        case 5 => wuerfelergebnis += "Joker!" + eol
+        }
+      }
+      return wuerfelergebnis
+    }
---
+++ src/main/scala/de/htwg/se/nochmal/model/Zahlwuerfel.scala
@@ -0,0 +1,27 @@
+package de.htwg.se.nochmal
+package model
+
+// Klasse für Zahlenwürfel
+case class numbers_dice(anzahl:Int = 3):
+  val wuerfelzahl = anzahl
+  
+  def roll_dice(): String =
+    if (anzahl < 1) {
+      return "Du brauchst mindestens einen Wuerfel!"
+    } else {
+      val randomizer = new scala.util.Random
+      val n = (1 to anzahl).toList
+      val diced_ints = n.map(i => randomizer.nextInt(6)+1)
+      
+      val eol = sys.props("line.separator")
+      var wuerfelergebnis = ""
+      for (x <- diced_ints) {
+        if (x == 6) {
+          wuerfelergebnis = wuerfelergebnis + "!" + eol
+        }
+        if (x < 6) {
+          wuerfelergebnis = wuerfelergebnis + x.toString() + eol
+        }
+      }
+      return wuerfelergebnis
+    }
