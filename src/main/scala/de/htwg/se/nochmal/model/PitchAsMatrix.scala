793275945d35d8d99031b6170fc595ad4a466400
TomkeV <lotvel03@gmail.com>
2023-11-10 08:52:19 +0100

    changed method to class

--- src/main/scala/de/htwg/se/nochmal/model/PitchAsMatrix.scala
+++ src/main/scala/de/htwg/se/nochmal/model/PitchAsMatrix.scala
@@ -1,12 +1,16 @@
 package de.htwg.se.nochmal
 package model
 
+case class PitchAsMatrix(matrix: Vector[Vector[Filling]]):
+  def this(rows:Int=4, columns:Int=7) = 
+    this(Vector.tabulate(rows) {i =>
+    Vector.tabulate(columns) {j => Filling.empty}
+  })
+
+
+
 // --- Methode, die neue leere Matrix mit n Zeilen und m Spalten erzeugt ---
-def create_Matrix(rows:Int = 4, columns:Int = 7): Vector[Vector[Filling]] =
+/* def create_Matrix(rows:Int = 4, columns:Int = 7): Vector[Vector[Filling]] =
 	Vector.tabulate(rows) { i =>
    	Vector.tabulate(columns) {j => Filling.empty}
-  }
-
-// --- Methode, die Vektor von Vektoren als solchen ausgibt ---
-/* def print_Matrix(m:Matrix) =
-	m.foreach(e => println(e.toString)) */
+  } */
