254a50d7af4a2830192bcf1348fae01bb8713e9f
TomkeV <lotvel03@gmail.com>
2023-11-10 09:12:35 +0100

    added vals, constructor, comments for methods

--- src/main/scala/de/htwg/se/nochmal/model/PitchAsMatrix.scala
+++ src/main/scala/de/htwg/se/nochmal/model/PitchAsMatrix.scala
@@ -2,12 +2,18 @@ package de.htwg.se.nochmal
 package model
 
 case class PitchAsMatrix(matrix: Vector[Vector[Filling]]):
-  def this(rows:Int=4, columns:Int=7) = 
+  val row_num = matrix.size 			// Anzahl Zeilen der Matrix
+  val col_nun = matrix.head.size	// Anzahl Spalten der Matrix
+
+	// Konstruktor
+  def this(rows:Int=4, columns:Int=7) =
     this(Vector.tabulate(rows) {i =>
     Vector.tabulate(columns) {j => Filling.empty}
-  })
+    })
+	
+	// Methode für Zugriff auf Index:
 
-
+	// Methode zum Füllen von Feldern:
 
 // --- Methode, die neue leere Matrix mit n Zeilen und m Spalten erzeugt ---
 /* def create_Matrix(rows:Int = 4, columns:Int = 7): Vector[Vector[Filling]] =
