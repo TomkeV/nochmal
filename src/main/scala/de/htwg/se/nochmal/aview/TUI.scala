c42980fbdedf54c4fcb58eb776bd8535f53e1eb1
TomkeV <lotvel03@gmail.com>
2023-11-28 18:47:29 +0100

    tidied up and added option to cross multiple field

--- src/main/scala/de/htwg/se/nochmal/aview/TUI.scala
+++ src/main/scala/de/htwg/se/nochmal/aview/TUI.scala
@@ -16,61 +16,67 @@ class TUI(controller: Controller) extends Observer:
   controller.add(this) // fügt TUI in die Liste hinzu, sodass update funktioniert
 
   var goOn = true
-
-// NEU  
+ 
   def run =
     println(controller.pitch.toString)
     //getInput()
     inputGetAndAnalysis()
-
-// NEU  
+ 
   override def update(e: Event) = 
       e match
         case Event.Quit => goOn = false
         case Event.Crossed => println(controller.pitch.toString) 
         case Event.Diced => println("Wo moechtest du ankreuzen? x Zahl Zahl")
 
-// NEU NEU
   def inputGetAndAnalysis():Unit =
     val input = readLine 
     input match
       case "q" => controller.publishQuit()
       case "w" => controller.publishDice()
       case _ => {
-        val chars = input.toCharArray()
+        val chars = input.toLowerCase().toCharArray()
+        val inputString = analyseChars(chars)
+        chars(0) match
+          case 'x' => controller.publishCross(inputString)
+          case ' ' => chars(1) match
+            case 'x' => controller.publishCross(inputString)
+          case _ => println("Ungueltige Eingabe!")
+        
+        
+        // TODO: controller.cross so umschreiben, dass String verarbeitet wird!
+/*           chars(0) match 
+            case 'x' => 
+              val line = chars(2).toString.toInt
+              val col = chars(4).toString.toInt
+              controller.publishCross(line, col) */
+      }
+    if goOn then inputGetAndAnalysis()
+
+
+  def analyseChars(array:Array[Char]):String = 
+    val range = Range(0, array.length).toList
+    val tmpArray = range.map(i =>
+          if(array(i).isDigit) {
+            array(i)
+          } else if (array(i).isLetter) {
+            'x'
+          } else {
+
+          }
+          )
+    var resString = ""
+    for (i <- 0 to array.length-1) {
+      if (tmpArray(i) != ()) {
+        resString += tmpArray(i)
+      }
+    }
+    return resString
+
+
+// case _ vorher:
+/*   val chars = input.toCharArray()
           chars(0) match 
             case 'x' => 
               val line = chars(2).toString.toInt
               val col = chars(4).toString.toInt
-              controller.publishCross(line, col)
-      }
-    if goOn then inputGetAndAnalysis()
-
-// Alte Methoden: 
-/*   def play(methodForInput: () => String):String =
-    val eol = sys.props("line.separator")
-    val dicecolors = Colors_dice(3)
-    val dicenumbers = Numbers_dice(3)
-    print("Bitte gib ein, was du tun moechtest.\n" +
-      "q: beenden\n" +
-      "w: wuerfeln\n" +
-      "x Zahl Zahl: Feld ankreuzen\n")
-    
-    val input = methodForInput()
-    input match
-      case "q" => "Danke fuers Spielen!"
-      case "w" => controller.dice()
-      case _ => val chars = input.toCharArray()
-                chars(0) match 
-                  case 'x' => 
-                    println("Zeile: " + chars(2).toString.toInt)
-                    //println("Spalte: " + chars(4).toString.toInt)
-                    controller.set(chars(2).toString.toInt, chars(4).toString.toInt)
-                    controller.toString
-*/
-
-/*   override def update() = printPitch  */
-
-/*   def printPitch = 
-    println(controller.pitch.toString)
-    println(play(readLine)) */
+              controller.publishCross(line, col) */
