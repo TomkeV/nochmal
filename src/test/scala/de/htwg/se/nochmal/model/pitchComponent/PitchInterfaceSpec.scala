4c7415542b686063e1f73f293f7f322fd418298c
TomkeV <lotvel03@gmail.com>
2024-01-12 09:13:38 +0100

    How to test Interface

---
+++ src/test/scala/de/htwg/se/nochmal/model/pitchComponent/PitchInterfaceSpec.scala
@@ -0,0 +1,14 @@
+/* package de.htwg.se.nochmal.model.pitchComponent
+
+
+// -----------------------------------------------------------------------------------------------------
+// --------------------------------------------------------------------------------------------- IMPORTS
+import org.scalatest.wordspec.AnyWordSpec
+import org.scalatest.matchers.should.Matchers._
+import de.htwg.se.nochmal.model.pitchComponent.baseModel.PitchAsMatrix
+
+// -----------------------------------------------------------------------------------------------------
+// ------------------------------------------------------------------------------------------------ TEST
+class PitchAsMatrixSpec extends AnyWordSpec {
+  given PitchInterface: PitchInterface = new PitchAsMatrix(7, 15)
+} */
