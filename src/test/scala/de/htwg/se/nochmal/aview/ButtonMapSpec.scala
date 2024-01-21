c2e1078b45ca151687a27ab71f2157d2e0fe493c
TomkeV <lotvel03@gmail.com>
2024-01-21 15:01:44 +0100

    da

--- src/test/scala/de/htwg/se/nochmal/aview/ButtonMapSpec.scala
+++ src/test/scala/de/htwg/se/nochmal/aview/ButtonMapSpec.scala
@@ -17,6 +17,7 @@ import controller.controllerComponent.ControllerInterface
 // Bibliotheksimports
 import org.scalatest.wordspec.AnyWordSpec
 import org.scalatest.matchers.should.Matchers._
+import scala.swing.Button
 
 
 
@@ -25,9 +26,10 @@ import org.scalatest.matchers.should.Matchers._
 class ButtonMapSpec extends AnyWordSpec {
   val controller = summon[ControllerInterface]
   "The ButtonMap " should {
-    val testMap = new ButtonMap(controller)
-    "implement the update to react to different events " in {
-      
+    val testMap = new ButtonMap(controller) 
+    "create a Map of buttons for a GUI saving their coordinates " in {
+      val mock = testMap.createMatrix
+      mock shouldBe a [Map[(Int, Int), Button]]
     }
   }
 }
